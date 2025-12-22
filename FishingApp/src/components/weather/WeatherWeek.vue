435f0ac402bd4507ae347d2446c47efefa01d055
maxim murashko <murashko199999@gmail.com>
2025-12-22 23:36:33 +0300

    change forecast-week

--- FishingApp/src/components/weather/WeatherWeek.vue
+++ FishingApp/src/components/weather/WeatherWeek.vue
@@ -1,19 +1,18 @@
 <template>
-  {{ weekWeather }}
   <div class="weather-conteiner">
     <div class="forecast-week">
       <div class="forecast-week__inner">
-        <BaseList :lists="forecastWearther" :heading="listHeading">
+        <BaseList :lists="weekWeather" :heading="listHeading">
           <template #list="{ list }">
             <div class="forecast-week__item">
               <p class="forecast-week__item-day">{{ list.day }}</p>
-              <p class="forecast-week__item-icon">{{ list.icon }}</p>
-              <p class="forecast-weel__item-temp">{{ list.temp }}</p>
+              <p class="forecast-week__item-icon">☀️</p>
+              <p class="forecast-week__item-temp">
+                {{ list.temp }}°<span>/{{ list.nightTemp }}°</span>
+              </p>
               <div class="forecast-week__item-metrics">
-                <p class="forecast-week__item-humidity">
-                  {{ list.humidity }}
-                </p>
-                <p class="forecast-week__item-result">{{ list.result }}</p>
+                <p class="forecast-week__item-chancerain">{{ list.chancerain }}%</p>
+                <p class="forecast-week__item-result">{{ list.sunrise }}</p>
               </div>
             </div>
           </template>
@@ -70,14 +69,19 @@ export default {
       if (!this.weatherWeek) return []
       const data = this.weatherWeek.daily.time.length
       let arr = []
-      console.log(data)
+      // console.log(data)
 
       for (let i = 0; i < data; i++) {
         arr.push({
-          time: this.weatherWeek.daily.time[i],
+          day: new Date(this.weatherWeek.daily.time[i]).toLocaleString('ru-RU', {
+            month: 'long',
+            day: 'numeric',
+          }),
           daylight: this.weatherWeek.daily.daylight_duration[i],
           temp: this.weatherWeek.daily.temperature_2m_max[i],
-          sunrise: this.weatherWeek.daily.sunrise[i],
+          sunrise: this.weatherWeek.daily.sunrise[i].slice(11, 16),
+          chancerain: this.weatherWeek.daily.precipitation_probability_max[i],
+          nightTemp: this.weatherWeek.daily.temperature_2m_min[i],
         })
       }
       console.log(arr)
@@ -89,4 +93,30 @@ export default {
   },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.forecast-week {
+  &__item {
+    border-radius: 18px;
+    background-color: #273e4b;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    gap: 18px;
+    &-metrics {
+      display: flex;
+      gap: 20px;
+      margin-left: auto;
+    }
+    &-temp {
+      font-size: 22px;
+      span {
+        font-size: 16px;
+        color: #b3b3b3;
+      }
+    }
+    &-icon {
+      font-size: 30px;
+    }
+  }
+}
+</style>
