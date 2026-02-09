dabeaf029d6176b6e76b010dc440f86a4fd54b84
maxim murashko <murashko199999@gmail.com>
2026-02-09 15:13:47 +0300

    change bait-result and ul li base list

--- FishingApp/src/pages/PageBait.vue
+++ FishingApp/src/pages/PageBait.vue
@@ -34,8 +34,23 @@
                 <BaseButton>Создать рецепт</BaseButton>
                 <BaseButton @click.prevent="formReload" variant="svg">
                   <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rotate-ccw h-4 w-4" aria-hidden="true"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path><path d="M3 3v5h5"></path></svg>
-                    </template>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-rotate-ccw h-4 w-4"
+                      aria-hidden="true"
+                    >
+                      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
+                      <path d="M3 3v5h5"></path>
+                    </svg>
+                  </template>
                 </BaseButton>
               </div>
             </div>
@@ -43,36 +58,64 @@
         </form>
       </div>
       <div class="bait__item">
-          <div class="bait__result">
-            <div class="bait__result-inner">
-              <div class="bait__result-title"></div>
-                <div class="bait__result-body">
-                  <div class="bait__result-structure">
-                    <ul>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
+        <div class="bait__result">
+          <div class="bait__result-inner">
+            <div class="bait__result-title">Рецепт прикормки</div>
+            <div class="bait__result-body">
+              <div class="bait__structure">
+                <div class="bait__structure-list">
+                  <p class="bait__structure-title">Состав:</p>
+                  <BaseList :lists="listStructure" variant="unstyled">
+                    <template #list="{ list }">
+                      <div class="bait__structure-item">
+                        <p class="bait__structure-name">{{ list.name }}</p>
+                        <span class="bait__structure-procent">{{ list.procent }}</span>
+                      </div>
+                    </template>
+                  </BaseList>
+                </div>
+              </div>
+              <div class="bait__cooked">
+                <div class="bait__cooked-list">
+                  <p class="bait__cooked-title">Приготовление:</p>
+                  <BaseList :lists="listCooked" variant="unstyled" as="ol">
+                    <template #list="{ list }">
+                      <div class="bait__cooked-item">
+                        <p class="bait__cooked-step">{{ list.step }}</p>
+                      </div>
+                    </template>
+                  </BaseList>
+                </div>
+              </div>
+              <div class="bait__options">
+                <div class="bait__options-items">
+                  <div class="bait__options-item">
+                    <div class="bait__options-inner">
+                      <div class="bait__options-key">Консистенция</div>
+                      <div class="bait__options-value">Средняя</div>
+                    </div>
                   </div>
-                  <div class="bait__result-cooked">
-                    <ul>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
+                  <div class="bait__options-item">
+                    <div class="bait__options-inner">
+                      <div class="bait__options-key">Связка</div>
+                      <div class="bait__options-value">Хорошая</div>
+                    </div>
                   </div>
-                  <div class="bait__result-options">
-                    123
-                  </div>
-                  <div class="bait__result-buttons">
-                    <BaseButton>Создать рецепт</BaseButton>
+                  <div class="bait__options-item">
+                    <div class="bait__options-inner">
+                      <div class="bait__options-key">Аромат</div>
+                      <div class="bait__options-value">Сладкий</div>
+                    </div>
                   </div>
                 </div>
-                <div class="bait__result-empty">123</div>
               </div>
+              <div class="bait__buttons">
+                <BaseButton variant="card">Сохранить пресет</BaseButton>
+              </div>
+            </div>
+            <!-- <div class="bait__result-empty">123</div> -->
           </div>
+        </div>
       </div>
     </div>
   </div>
@@ -81,14 +124,30 @@
 import BaseSelect from '@/components/ui/BaseSelect.vue'
 import BaseRange from '@/components/ui/BaseRange.vue'
 import BaseButton from '@/components/ui/BaseButton.vue'
+import BaseList from '@/components/ui/BaseList.vue'
 export default {
   components: {
     BaseSelect,
     BaseRange,
     BaseButton,
+    BaseList,
   },
   data() {
     return {
+      listCooked: [
+        { step: 'Замочить кукурузную крупу на 2 часа' },
+        { step: 'Перемешать сухие компоненты' },
+        { step: 'Добавить ароматизатор' },
+        { step: 'Увлажнить водой из водоёма' },
+        { step: 'Слепить шары размером с теннисный мяч' },
+      ],
+      listStructure: [
+        { name: 'Кукурузная крупа', procent: '40%' },
+        { name: 'Панировочные сухари', procent: '30%' },
+        { name: 'Жмых подсолнечный', procent: '15%' },
+        { name: 'Манная крупа', procent: '10%' },
+        { name: 'Ванильный ароматизатор', procent: '5мл' },
+      ],
       formData: this.getDefaultFormData(),
       formFields: [
         {
@@ -211,17 +270,100 @@ export default {
     flex: 1 1 50%;
     max-width: 50%;
   }
+  &__result {
+    background-color: #1b3a4b;
+    padding: 24px;
+    border-radius: 18px;
+    border: 1px solid #4b5563;
+    &-inner {
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+    }
+    &-body {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+  }
+  &__cooked {
+    &-list {
+      padding-bottom: 16px;
+      border-bottom: 1px solid #525a68;
+    }
+    &-title {
+      margin-bottom: 8px;
+    }
+    &-step {
+      font-size: 14px;
+      line-height: 1.4;
+    }
+  }
+  &__structure {
+    &-list {
+      padding-bottom: 16px;
+      border-bottom: 1px solid #525a68;
+    }
+    &-title {
+      margin-bottom: 8px;
+    }
+    &-item {
+      background-color: #8d654c;
+      border-radius: 4px;
+      color: #fff;
+      display: flex;
+      justify-content: space-between;
+      padding: 8px;
+      font-size: 14px;
+      line-height: 1.4;
+    }
+    &-procent {
+      border: 1px solid #4b5563;
+      padding: 2px 8px;
+      border-radius: 12px;
+      font-size: 12px;
+      line-height: 1;
+      display: flex;
+      align-items: center;
+    }
+  }
+  &__options {
+    &-items {
+      display: flex;
+      justify-content: space-between;
+    }
+    &-item {
+      flex: 1 1 33.333%;
+      max-width: 33.333%;
+    }
+    &-inner {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 12px;
+    }
+    &-key {
+      color: #b3b3b3;
+      font-size: 14px;
+      line-height: 1.4;
+    }
+    &-value {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 1.5;
+    }
+  }
 }
 .form {
   background-color: #1b3a4b;
   padding: 24px;
   border-radius: 18px;
   border: 1px solid #4b5563;
-  &__buttons{
-    &-wrapper{
+  &__buttons {
+    &-wrapper {
       display: flex;
       gap: 12px;
-      & > *:first-child{
+      & > *:first-child {
         flex: 1;
       }
     }
